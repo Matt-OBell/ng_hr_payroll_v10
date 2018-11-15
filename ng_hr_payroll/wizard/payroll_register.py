883b1d8d314c3d721332555322d072337f7147aa
obabawale <oebabawale@rocketmail.com>
2018-11-15 09:56:48 +0100

    Remove redundant code lines

--- ng_hr_payroll/wizard/payroll_register.py
+++ ng_hr_payroll/wizard/payroll_register.py
@@ -40,18 +40,7 @@ class payroll_reg(models.TransientModel):
     employee_ids = fields.Many2many('hr.employee', 'payroll_register_rel', 'payroll_year_id', 'employee_id', string='Employees', required=True)
     rule_ids = fields.Many2many('hr.salary.rule', 'payroll_register_rel_salary', 'reg_id', 'rule_id', string='Salary Rules', required=True)
     xls_output = fields.Boolean(string='Excel Output', help='Tick if you want to output of report in excel sheet')
-#        'category_id': fields.many2one('hr.salary.rule.category', 'Category', required=True),
 
-#    def _get_default_category(self, cr, uid, context=None):
-#        category_ids = self.pool.get('hr.salary.rule.category').search(cr, uid, [('code', '=', 'NET')], context=context)
-#        return category_ids and category_ids[0] or False
-
-#     _defaults = {
-#         'start_date': lambda *a: time.strftime('%Y-01-01'),
-#         'end_date': lambda *a: time.strftime('%Y-%m-%d'),
-#         'category_id': _get_default_category
-#     }
-#         'end_date': lambda *a: time.strftime('%Y-%m-%d'),
     @api.multi
     def print_report(self, data):
         """
@@ -62,9 +51,10 @@ class payroll_reg(models.TransientModel):
          @param context: A standard dictionary
          @return: return report
         """
+
+        print("============================== Say cheese!!! ================================================")
         context = (self._context or {})
         datas = {'ids': context.get('active_ids', [])}
-
         res = self.read()
         res = res and res[0] or {}
         datas.update({'form': res})
@@ -78,7 +68,6 @@ class payroll_reg(models.TransientModel):
             al = xlwt.Alignment()
             al.horz = xlwt.Alignment.HORZ_CENTER
             title_style.alignment = al
-            
             sheet.write_merge(0, 0, 5, 9, 'Payroll Register', title_style)
             sheet.write(1, 6, datas['form']['name'], title_style1)
             sheet.write(2, 4, 'From', title_style1)
@@ -87,7 +76,6 @@ class payroll_reg(models.TransientModel):
             sheet.write(2, 7, datas['form']['end_date'], title_style1)
             main_header = obj_pr.get_periods(datas['form']) 
             row = self.render_header(sheet, ['Name'] + main_header[0] + ['Total'], first_row=5)
-
             emp_datas = obj_pr.get_employee(datas['form'], excel=True)
             value_style = xlwt.easyxf('font: name Helvetica,bold on', num_format_str='#,##0.00')
             cell_count = 0
@@ -98,61 +86,24 @@ class payroll_reg(models.TransientModel):
                 row += 1
                 cell_count = 0
             sheet.write(row+1, 0, 'Total', value_style)
-
             total_datas = obj_pr.get_months_tol()
             cell_count = 1
             row += 1
             for record in total_datas[-1][1:]:
                 sheet.write(row, cell_count, record, value_style)
                 cell_count += 1
-                # row += 1
-                # cell_count = 0
-                # sheet.write(row+1, cell_count, v, value_style)
-
-
-
-
-
-
-
-
-            # total_datas = obj_pr.get_months_tol()
-            # cell_count = 1
-            # row += 1
-
-            # for value in total_datas:
-            #     for v in value[1:]:
-            #         sheet.write(row, cell_count, v, value_style)
-            #         print "sheet", sheet
-            #         cell_count += 1
-                # row += 1
-                # cell_count = 0
             stream = BytesIO()
             workbook.save(stream)
-            ctx = {'default_xls_output': base64.encodebytes(stream.getvalue())}
             ir_attachment = self.env['ir.attachment'].create({
                 'name': self.name + '.xls',
                 'datas': base64.encodebytes(stream.getvalue()),
                 'datas_fname': self.name + '.xls'}).id
-
-            # actid = self.env.get('ir.model.data').get_object_reference(cr, uid, 'base', 'action_attachment')[1]
             actid = self.env.ref('base.action_attachment')[0]
             myres = actid.read()[0]
             myres['domain'] = "[('id','in',[" + ','.join(map(str, [ir_attachment])) + "])]"
+            print("++++++++++ Hello there ++++++++++++++++")
             return myres
-            return {
-                'context': ctx,
-                'view_type': 'form',
-                'view_mode': 'form',
-                'res_model': 'account.xls.output.wiz',
-                'type': 'ir.actions.act_window',
-                'target': 'new',
-                }
-#        return {
-#            'type': 'ir.actions.report.xml',
-#            'report_name': 'hr.payroll.register',
-#            'datas': datas,
-#       }
+        print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
         return self.env['report'].get_action(self, 'ng_hr_payroll.payroll_register_report', data=datas)
 
     @api.multi
@@ -163,6 +114,4 @@ class payroll_reg(models.TransientModel):
             ws.write(first_row, col, hdr, header_style)
             col += 1
         return first_row + 2
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
